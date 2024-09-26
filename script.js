b28c07e3fe8bc602174d16978e80c294095f735a
Basirat Hamayoon <basirathumayun@gmail.com>
2024-09-26 14:26:01 +0500

    make function for remove list and complete task

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let inputs = document.getElementById('text-Input');
 let text = document.querySelector('.tasks');
 var addIcon = document.getElementById('image-icon')
-{
+addIcon = addIcon.addEventListener('click', function()
+ {
     if(inputs.value == ""){
         alert("Todo can't be empty!");
+    }else{
+        let newElement = document.createElement("li");
+        newElement.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        console.log(newElement);
+        text.appendChild(newElement);
+        inputs.value="";
+        newElement.querySelector("i").addEventListener("click", remove);
+        newElement.onclick = function(){
+            newElement.classList.toggle('completd')
+        }
+        function remove(){
+            newElement.remove();
+        }
     }
-}
+ });
+// console.log(text);
