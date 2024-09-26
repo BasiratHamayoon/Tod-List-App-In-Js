199d7da0dc36e39d0adae90dd69d83ebfa7ac39b
Basirat Hamayoon <basirathumayun@gmail.com>
2024-09-26 14:17:17 +0500

    Declear an event listner function on image and also use if statment for empty todo

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let inputs = document.getElementById('text-Input');
 let text = document.querySelector('.tasks');
 var addIcon = document.getElementById('image-icon')
+{
+    if(inputs.value == ""){
+        alert("Todo can't be empty!");
+    }
+}
