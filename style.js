6a097d960e34de928518974024395fa618f4d175
Basirat Hamayoon <basirathumayun@gmail.com>
2024-09-26 14:13:04 +0500

    do a little bit styling in it

---
+++ style.js
@@ -0,0 +1,43 @@
+body{
+    background: radial-gradient(rgb(173, 207, 246), rgb(199, 233, 237), rgb(213, 234, 246));
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.container{
+    width: 300px;
+    height: 400px;
+    background-color: rgba(20, 96, 219, 0.712);
+    margin: auto;
+    text-align: center;
+    overflow: scroll;
+    padding: 8px;
+}
+.container input{
+    width: 220px;
+    height: 40px;
+    border: none;
+    padding-left: 6px;
+    background-color: #ffffff85;
+}
+.container input:focus{
+    outline: none;
+    border: none;
+}
+.container img{
+    position: absolute;
+    margin-left: -30px;
+    margin-top: 5px;
+}
+.container ul li{
+    list-style: none;
+    background-color: rgba(234, 231, 227, 0.843);
+    width: 210px;
+    height: 30px;
+    text-align: start;
+    margin-left: -10px;
+    padding: 8px;
+    margin-top: 10px;
+}
+.completd{
+    list-style: square;
+    text-decoration: line-through;
+}
